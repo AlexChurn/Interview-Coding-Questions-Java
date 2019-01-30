79e556b669f1482c7563ef615e8787ea9cb170ac
AlexChurn <31522923+AlexChurn@users.noreply.github.com>
2019-01-29 22:17:26 -0500

    Add files via upload

---
+++ CharReplace.js
@@ -0,0 +1,22 @@
+public class HelloWorld{
+
+     public static void main(String []args){
+        String srt = "poor";
+        int length = srt.length();
+        System.out.println("The word is " + srt + ".");
+        charReplace(srt, 'l', 0);
+    }
+
+    static void charReplace(String str, char ch, int c){
+        try{
+            if(str.charAt(c) == 'r')
+                System.out.println("The new word is " + str.replace('r', 'l')+".");
+            c++;
+            charReplace(str, ch, c);
+
+        }catch(StringIndexOutOfBoundsException e)
+        {
+            System.out.println("No more chars in the string!");
+        }
+    }
+}
